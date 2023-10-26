ff55e666cbc7ca887bdd1751a57ae100371271c4
rneacy <rneacy@gmail.com>
2023-10-26 18:36:10 +0100

    slots and induction smelter maybe

--- ccscripts/oremachine.lua
+++ ccscripts/oremachine.lua
@@ -8,6 +8,9 @@ local barrels = {
     item_redstone = peripheral.wrap("yabba:item_barrel_0"),
     tile_ore_cinnabar = peripheral.wrap("yabba:item_barrel_1"),
     item_thermalfoundation_material_crystalCinnabar = peripheral.wrap("yabba:item_barrel_2"),
+    item_blazeRod = peripheral.wrap("yabba:item_barrel_3"),
+    item_thermalfoundation_material_dustSulfur = peripheral.wrap("yabba:item_barrel_4"),
+    item_snowball = peripheral.wrap("yabba:item_barrel_5"),
 }
 
 
@@ -17,27 +20,41 @@ local inventories = {
         name = "pyro crafter",
         block = peripheral.wrap("rftools:crafter1_0"),
         requirements = {
-            "item.redstone",
-            "item.blazeRod",
-            "item.thermalfoundation.material.dustSulfur",
-        },
-        delegates = {
+            item_redstone = {},
             item_blazeRod = {
-                name = "item.blazePowder",
-                amount_made = 4,
-            }
-        }
+                delegate = {
+                    name = "item_blazePowder",
+                    amount_made = 4,
+                },
+            },
+            item_thermalfoundation_material_dustSulfur = {},
+        },
     },
     {
         name = "cryo crafter",
         block = peripheral.wrap("rftools:crafter1_1"),
         requirements = {
-            "item.redstone",
-            "item.snowball",
+            item_redstone = {},
+            item_snowball = {
+                push_times = 4,
+            },
         }
     },
+    {
+        name = "induction smelter",
+        block = peripheral.wrap("thermalexpansion:machine_smelter_0"),
+        requirements = {
+            item_thermalfoundation_material_crystalCinnabar = {
+                slot = 1,
+            }
+        },
+    }
 }
 
+-- Replace . with _ for stinky lua tables
+local function nameconv(name)
+    return name:gsub("%.", "_")
+end
 
 while true do
     for _, inv in pairs(inventories) do
@@ -48,28 +65,27 @@ while true do
         -- Determine what is in inventory now
         for i, slot in pairs(slots_taken) do
             local stored_item = inv.block.getItemMeta(i)
-            local si_name = stored_item.rawName
+            local si_name = nameconv(stored_item.rawName)
             local si_count = stored_item.count
             inv_contents[si_name] = (inv_contents[si_name] or 0) + si_count
         end
 
         -- Two stacks of a particular item allowed at one time
-        for _, requirement in pairs(inv.requirements) do
-            local req_safe_name = requirement:gsub("%.", "_")
+        for requirement, options in pairs(inv.requirements) do
             local og_requirement = requirement
+            
+            local multiplier = 1
 
-            -- If a requirement is actually made by something else
-            local delegate = inv.delegates[req_safe_name]
-            local divisor = 1
-            if delegate then
-                requirement = delegate.name
-                divisor = delegate.amount_made
+             -- If a requirement is actually made by something else
+            if options.delegate then
+                requirement = options.delegate.name
+                multiplier = options.delegate.amount_made / 2
             end
 
-            local push_limit = 64 / divisor
+            local push_limit = 64 * multiplier
 
             if not inv_contents[requirement] or inv_contents[requirement] < push_limit then
-                local barrel = barrels[req_safe_name]
+                local barrel = barrels[requirement]
 
                 if barrel then
                     if not barrel.list() then
@@ -79,13 +95,15 @@ while true do
 
                         print("Pushing "..og_requirement.." to "..block_name)
 
-                        local times
-                        if og_requirement == "item.snowball" then times = 4 else times = 1 end
+                        local times = 1
+                        if options.push_times then times = options.push_times end
 
                         for i = 1, times do
                             barrel.pushItems(
                                 block_name,
-                                BARREL_SLOT
+                                BARREL_SLOT,
+                                nil,
+                                options.slot
                             )
                         end
                     end
@@ -94,7 +112,6 @@ while true do
                 end
             end
         end
-
-        os.sleep(1)
     end
+    os.sleep(1)
 end
